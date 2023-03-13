ef6829abdee944c510751e5a7d2b5183f9755cb9
J. McBride <j.mcbride@Js-MacBook-Pro-2.local>
2023-03-13 01:58:18 -0400

    edit text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
       <div className="container px-5 py-10 mx-auto lg:px-40">
         <div className="flex flex-col w-full mb-10">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white md:text-center">
-            Featured Recent Projects
+            Featured Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-left md:text-center">
             Most projects developed for clients of{" "}
